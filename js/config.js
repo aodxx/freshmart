494f42b0703f7c978e005febf6b74d14537a8274
aodxx <aodaod3826@gmail.com>
2026-07-27 05:56:45 +0800

    fix: correct FreshMart LIFF ID

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ export const CONFIG = Object.freeze({
   SUPABASE_URL: 'https://jilaasxicogktwrjnkmu.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_MAh-wQQEjRX1ocTON_iNsA_EA04QgxS',
   APP_BASE_PATH: '/freshmart/',
-  LIFF_ID: '2010025658-kBKgsnz',
+  LIFF_ID: '2010025658-kBKgsnzH',
   LIFF_API_URL: 'https://jilaasxicogktwrjnkmu.supabase.co/functions/v1/liff-api',
   STORAGE_BUCKET: 'payment-slips',
   CURRENCY: 'THB',
