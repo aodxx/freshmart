b9cbf1ad582b9ec1c2d9704a41a2da9cef2b6bcf
aodxx <aodaod3826@gmail.com>
2026-07-27 03:33:43 +0800

    fix: keep auth redirects within FreshMart Pages path

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 export const CONFIG = Object.freeze({
   SUPABASE_URL: 'https://jilaasxicogktwrjnkmu.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_MAh-wQQEjRX1ocTON_iNsA_EA04QgxS',
+  APP_BASE_PATH: '/freshmart/',
   STORAGE_BUCKET: 'payment-slips',
   CURRENCY: 'THB',
   LOCALE: 'th-TH'
